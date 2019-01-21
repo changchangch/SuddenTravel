fcff2814b99b3b4eb1c751b46acc1553ce816de9
changchangch <45754721+changchangch@users.noreply.github.com>
2019-01-22 01:47:14 +0800

    Add files via upload

--- randjs/test52.js
+++ randjs/test52.js
@@ -1,6 +1,6 @@
 var imgnumber = Math.floor(Math.random()*9);
-var imgurl = ['./摩天輪/縮小/10.jpg', './摩天輪/縮小/11.jpg', './摩天輪/縮小/12.jpg', './摩天輪/縮小/13.jpg', './摩天輪/縮小/14.jpg', './摩天輪/縮小/15.jpg',
-			  './摩天輪/縮小/16.jpg', './摩天輪/縮小/17.jpg', './摩天輪/縮小/18.jpg'];
-var imgurlind = ['./摩天輪html/EDAWorld.html', './摩天輪html/EDAWorld.html', './摩天輪html/EDAWorld.html', './摩天輪html/TCAP.html', './摩天輪html/TCAP.html', './摩天輪html/TCAP.html',
-				 './摩天輪html/WindowonChina.html', './摩天輪html/WindowonChina.html', './摩天輪html/DreamMall.html']
+var imgurl = ['./摩天輪/縮小/02.jpg', './摩天輪/縮小/05.jpg', './摩天輪/縮小/08.jpg', './摩天輪/縮小/11.jpg', './摩天輪/縮小/14.jpg', './摩天輪/縮小/17.jpg',
+			  './摩天輪/縮小/20.jpg', './摩天輪/縮小/23.jpg', './摩天輪/縮小/26.jpg'];
+var imgurlind = ['./摩天輪html/skydream.html', './摩天輪html/JanfusunFancyworld.html', './摩天輪html/Miramar.html', './摩天輪html/EDAWorld.html', './摩天輪html/TCAP.html', './摩天輪html/WindowonChina.html',
+				 './摩天輪html/TAROKOPARK.html', './摩天輪html/HualienFargloryOceanPark.html', './摩天輪html/MitsuiOutletPark.html']
 document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
